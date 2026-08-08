c0f2f96a8e499597dc0097f54c38e36d2a325b52
fatalbrux <henry.quispe@sistemas.edu.bo>
2026-08-08 13:41:00 -0400

    Agregando detalle ciertas operaciones en equipo e instalacion

--- src/modules/instalacion/instalacion.module.ts
+++ src/modules/instalacion/instalacion.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { InstalacionService } from './instalacion.service';
 import { InstalacionController } from './instalacion.controller';
 import { Instalacion } from './entities/instalacion.entity';
-
+import { Equipo } from '../equipo/entities/equipo.entity';
+import { Cliente } from '../cliente/entities/cliente.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([Instalacion])],
+  imports: [TypeOrmModule.forFeature([Instalacion, Equipo, Cliente])],
   controllers: [InstalacionController],
   providers: [InstalacionService],
   exports: [TypeOrmModule],
